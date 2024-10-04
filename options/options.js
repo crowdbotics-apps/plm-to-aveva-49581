77a4cc7b7b2676ce8a88658627ae498311b55545
Crowdbotics <team@crowdbotics.com>
2024-10-04 14:13:17 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "plm-to-aveva-49581",
+  url: "https://plm-to-aveva-49581.botics.co",
+  api: "https://plm-to-aveva-49581.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
